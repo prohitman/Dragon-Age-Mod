95bb82662f986290ca50698069bbb8d4a3496f83
prohitman <43189387+prohitman@users.noreply.github.com>
2020-05-20 15:51:05 +0000

    Add files via upload

--- main/java/com/prohitman/dragonage/client/gui/ForgeringTableScreen.java
+++ main/java/com/prohitman/dragonage/client/gui/ForgeringTableScreen.java
@@ -12,39 +12,39 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
-public class ForgeringTableScreen extends ContainerScreen<ForgeringTableContainer>
-{
-	public static final ResourceLocation BACKGROUND_TEXTURE = new ResourceLocation(DragonAge.MOD_ID, "textures/gui/forging_table_gui.png");
-	
+public class ForgeringTableScreen extends ContainerScreen<ForgeringTableContainer> {
+	public static final ResourceLocation BACKGROUND_TEXTURE = new ResourceLocation(DragonAge.MOD_ID,
+			"textures/gui/forging_table_gui.png");
+
 	public ForgeringTableScreen(ForgeringTableContainer screenContainer, PlayerInventory inv, ITextComponent titleIn) {
 		super(screenContainer, inv, titleIn);
 		this.guiLeft = 0;
 		this.guiTop = 0;
-		this.xSize = 176;
-		this.ySize = 166;
 	}
-	
+
 	@Override
 	public void render(final int mouseX, final int mouseY, final float partialTicks) {
 		this.renderBackground();
 		super.render(mouseX, mouseY, partialTicks);
 		this.renderHoveredToolTip(mouseX, mouseY);
 	}
-	
+
 	@Override
 	protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
 		super.drawGuiContainerForegroundLayer(mouseX, mouseY);
 		this.font.drawString(this.title.getFormattedText(), 8.0f, 6.0f, 4210752);
-		//this.font.drawString(this.playerInventory.getDisplayName().getFormattedText(), 8.0f, 90.0f, 4210752);
+		// this.font.drawString(this.playerInventory.getDisplayName().getFormattedText(),
+		// 8.0f, 90.0f, 4210752);
 	}
-	
+
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
 		RenderSystem.color4f(1.0f, 1.0f, 1.0f, 1.0f);
 		this.minecraft.getTextureManager().bindTexture(BACKGROUND_TEXTURE);
-		int x = (this.width - this.xSize) / 2;
-		int y = (this.height - this.ySize) / 2;
-		this.blit(x, y, 0, 0, this.xSize, this.ySize);
+		int i = (this.width - 176) / 2;
+		int j = (this.height - 190) / 2;
+		this.blit(i, j, 0, 0, 175, 189);
 	}
+	
 	
 }
--- main/java/com/prohitman/dragonage/containers/ForgeringTableContainer.java
+++ main/java/com/prohitman/dragonage/containers/ForgeringTableContainer.java
@@ -1,24 +1,19 @@
 package com.prohitman.dragonage.containers;
 
-import java.util.Objects;
 import java.util.Optional;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import com.prohitman.dragonage.init.ModBlocks;
 import com.prohitman.dragonage.init.ModContainerTypes;
 import com.prohitman.dragonage.recipes.ForgeringTableRecipes;
 import com.prohitman.dragonage.recipes.IModRecipeTypes;
-import com.prohitman.dragonage.tileentities.ForgeringTableTileEntity;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.CraftResultInventory;
+import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.IRecipeHelperPopulator;
-import net.minecraft.inventory.Inventory;
+import net.minecraft.inventory.container.CraftingResultSlot;
 import net.minecraft.inventory.container.RecipeBookContainer;
 import net.minecraft.inventory.container.Slot;
 import net.minecraft.item.ItemStack;
@@ -26,116 +21,90 @@ import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.RecipeItemHelper;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.play.server.SSetSlotPacket;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IWorldPosCallable;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class ForgeringTableContainer extends RecipeBookContainer<IInventory>
-{
-	@SuppressWarnings("unused")
-	private static final Logger LOGGER = LogManager.getLogger();
-	private final IInventory outputSlot = new CraftResultInventory();
-	private final IInventory inputSlots = new Inventory(2);
+public class ForgeringTableContainer extends RecipeBookContainer<CraftingInventory> {
+	private final CraftResultInventory outputSlot = new CraftResultInventory();
+	private final CraftingInventory inputSlots = new CraftingInventory(this, 2, 1);
 	private final IWorldPosCallable canInteractwithCallable;
-	@SuppressWarnings("unused")
-	private final ForgeringTableTileEntity tileEntity;
 	private final PlayerEntity player;
 
-	public ForgeringTableContainer(final int windowid, final PlayerInventory playerInventory, final ForgeringTableTileEntity tileEntity)
-	{
+	public ForgeringTableContainer(final int windowid, final PlayerInventory playerInventory, IWorldPosCallable pos) {
 		super(ModContainerTypes.FORGERING_TABLE_CONTAINER.get(), windowid);
-		this.tileEntity = tileEntity;
-		this.canInteractwithCallable = IWorldPosCallable.of(tileEntity.getWorld(), tileEntity.getPos());
+		this.canInteractwithCallable = pos;
 		this.player = playerInventory.player;
-		
-		//FT's inventory
+
+		// FT's inventory
 		this.addSlot(new Slot(this.inputSlots, 0, 27, 47));
 		this.addSlot(new Slot(this.inputSlots, 1, 76, 47));
-		this.addSlot(new Slot(this.outputSlot, 2, 134, 47) {
-			
-	         public boolean isItemValid(ItemStack stack) {
-	            return false;
-	         }
+		this.addSlot(new CraftingResultSlot(playerInventory.player, this.inputSlots, this.outputSlot, 2, 134, 47));
 
-	         /**
-	          * Return whether this slot's stack can be taken from this slot.
-	          */
-	         public boolean canTakeStack(PlayerEntity playerIn) {
-	            return true;
-	         }
-	         
-		});
-		
-		//Main Inventory
-		for(int i = 0; i < 3; ++i) {
-	         for(int j = 0; j < 9; ++j) 
-	         {
-	            this.addSlot(new Slot(playerInventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
-	         }
-	      }
-		
-		//HotBar
-	      for(int k = 0; k < 9; ++k) 
-	      {
-	         this.addSlot(new Slot(playerInventory, k, 8 + k * 18, 142));
-	      }
-		
+		// Main Inventory
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 9; ++j) {
+				this.addSlot(new Slot(playerInventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+			}
+		}
+
+		// HotBar
+		for (int k = 0; k < 9; ++k) {
+			this.addSlot(new Slot(playerInventory, k, 8 + k * 18, 142));
+		}
+
 	}
-	
-	protected static void func_217066_a(int p_217066_0_, World world, PlayerEntity player, IInventory inventory, CraftResultInventory result) {
+
+	protected static void updatecraftmatrix(int id, World world, PlayerEntity player, CraftingInventory inventory,
+			CraftResultInventory result) {
 		if (!world.isRemote) {
-			ServerPlayerEntity serverplayerentity = (ServerPlayerEntity)player;
+			ServerPlayerEntity serverplayerentity = (ServerPlayerEntity) player;
 			ItemStack itemstack = ItemStack.EMPTY;
-			Optional<ForgeringTableRecipes> optional = world.getServer().getRecipeManager().getRecipe(IModRecipeTypes.FORGERING, inventory, world);
+			Optional<ForgeringTableRecipes> optional = world.getServer().getRecipeManager()
+					.getRecipe(IModRecipeTypes.FORGERING, inventory, world);
 			if (optional.isPresent()) {
-			ForgeringTableRecipes icraftingrecipe = optional.get();
-        	 if (result.canUseRecipe(world, serverplayerentity, icraftingrecipe)) {
-        		 itemstack = icraftingrecipe.getCraftingResult(inventory);
-        	 }
+				ForgeringTableRecipes icraftingrecipe = optional.get();
+				if (result.canUseRecipe(world, serverplayerentity, icraftingrecipe)) {
+					itemstack = icraftingrecipe.getCraftingResult(inventory);
+				}
 			}
 
-			result.setInventorySlotContents(0, itemstack);
-			serverplayerentity.connection.sendPacket(new SSetSlotPacket(p_217066_0_, 0, itemstack));
+			result.setInventorySlotContents(2, itemstack);
+			serverplayerentity.connection.sendPacket(new SSetSlotPacket(id, 2, itemstack));
 		}
 	}
-	
-	public ForgeringTableContainer(final int windowId, final PlayerInventory playerInventory, final PacketBuffer data) 
-	{
-		this(windowId, playerInventory, getTileEntity(playerInventory, data));
+
+	public ForgeringTableContainer(final int windowid, final PlayerInventory playerinv, PacketBuffer data) {
+		this(windowid, playerinv, IWorldPosCallable.DUMMY);
 	}
-	
+
 	public void onCraftMatrixChanged(IInventory inventoryIn) {
-	      this.canInteractwithCallable.consume((p_217069_1_, p_217069_2_) -> {
-	         func_217066_a(this.windowId, p_217069_1_, this.player, this.inputSlots, (CraftResultInventory)this.outputSlot);
-	      });
-	   }
-	
+		this.canInteractwithCallable.consume((world, pos) -> {
+			updatecraftmatrix(this.windowId, world, this.player, (CraftingInventory) this.inputSlots, this.outputSlot);
+		});
+	}
+
 	@Override
 	public boolean canInteractWith(PlayerEntity playerIn) {
 		return isWithinUsableDistance(canInteractwithCallable, playerIn, ModBlocks.FORGERING_TABLE.get());
 	}
-	
-	private static ForgeringTableTileEntity getTileEntity(final PlayerInventory playerInventory, final PacketBuffer data)
-	{
-		Objects.requireNonNull(playerInventory, "playerInventory cannot be null");
-		Objects.requireNonNull(data, "data cannot be null");
-		final TileEntity tileAtPos = playerInventory.player.world
-				.getTileEntity(data.readBlockPos());
-		if(tileAtPos instanceof ForgeringTableTileEntity)
-		{
-			return (ForgeringTableTileEntity)tileAtPos;
-		}
-		
-		throw new IllegalStateException("Tile Entity is not correct" + tileAtPos);
-	}
+
+//	private static ForgeringTableTileEntity getTileEntity(final PlayerInventory playerInventory,
+//			final PacketBuffer data) {
+//		Objects.requireNonNull(playerInventory, "playerInventory cannot be null");
+//		Objects.requireNonNull(data, "data cannot be null");
+//		final TileEntity tileAtPos = playerInventory.player.world.getTileEntity(data.readBlockPos());
+//		if (tileAtPos instanceof ForgeringTableTileEntity) {
+//			return (ForgeringTableTileEntity) tileAtPos;
+//		}
+//
+//		throw new IllegalStateException("Tile Entity is not correct" + tileAtPos);
+//	}
 
 	@Override
 	public void fillStackedContents(RecipeItemHelper itemHelperIn) {
-		if (this.inputSlots instanceof IRecipeHelperPopulator) {
-	         ((IRecipeHelperPopulator)this.inputSlots).fillStackedContents(itemHelperIn);
-	      }
+		this.inputSlots.fillStackedContents(itemHelperIn);
 	}
 
 	@Override
@@ -145,76 +114,86 @@ public class ForgeringTableContainer extends RecipeBookContainer<IInventory>
 	}
 
 	@Override
-	public boolean matches(IRecipe<? super IInventory> recipeIn) {
+	public boolean matches(IRecipe<? super CraftingInventory> recipeIn) {
 		return recipeIn.matches(this.inputSlots, this.player.world);
 	}
 
 	@Override
 	public int getOutputSlot() {
-		return 1;
+		return 2;// this.outputSlot.getSizeInventory();
 	}
 
 	@Override
 	public int getWidth() {
-		return 1;
+		return this.inputSlots.getWidth();
 	}
 
 	@Override
 	public int getHeight() {
-		return 1;
+		return this.inputSlots.getHeight();
 	}
 
 	@OnlyIn(Dist.CLIENT)
-	public int getSize() 
-	{
+	public int getSize() {
 		return 3;
 	}
-	
+
+	/**
+	 * Called to determine if the current slot is valid for the stack merging
+	 * (double-click) code. The stack passed in is null for the initial slot that
+	 * was double-clicked.
+	 */
+	public boolean canMergeSlot(ItemStack stack, Slot slotIn) {
+		return slotIn.inventory != this.outputSlot && super.canMergeSlot(stack, slotIn);
+	}
+
+	/**
+	 * Handle when the stack in slot {@code index} is shift-clicked. Normally this
+	 * moves the stack between the player inventory and the other inventory(s).
+	 */
 	public ItemStack transferStackInSlot(PlayerEntity playerIn, int index) {
-	      ItemStack itemstack = ItemStack.EMPTY;
-	      Slot slot = this.inventorySlots.get(index);
-	      if (slot != null && slot.getHasStack()) {
-	         ItemStack itemstack1 = slot.getStack();
-	         itemstack = itemstack1.copy();
-	         if (index == 0) {
-	            this.canInteractwithCallable.consume((p_217067_2_, p_217067_3_) -> {
-	               itemstack1.getItem().onCreated(itemstack1, p_217067_2_, playerIn);
-	            });
-	            if (!this.mergeItemStack(itemstack1, 10, 46, true)) {
-	               return ItemStack.EMPTY;
-	            }
+		ItemStack itemstack = ItemStack.EMPTY;
+		Slot slot = this.inventorySlots.get(index);
+		if (slot != null && slot.getHasStack()) {
+			ItemStack itemstack1 = slot.getStack();
+			itemstack = itemstack1.copy();
+			if (index == 2) {
+				if (!this.mergeItemStack(itemstack1, 3, 39, true)) {
+					return ItemStack.EMPTY;
+				}
 
-	            slot.onSlotChange(itemstack1, itemstack);
-	         } else if (index >= 10 && index < 46) {
-	            if (!this.mergeItemStack(itemstack1, 1, 10, false)) {
-	               if (index < 37) {
-	                  if (!this.mergeItemStack(itemstack1, 37, 46, false)) {
-	                     return ItemStack.EMPTY;
-	                  }
-	               } else if (!this.mergeItemStack(itemstack1, 10, 37, false)) {
-	                  return ItemStack.EMPTY;
-	               }
-	            }
-	         } else if (!this.mergeItemStack(itemstack1, 10, 46, false)) {
-	            return ItemStack.EMPTY;
-	         }
+				slot.onSlotChange(itemstack1, itemstack);
+			} else if (index != 0 && index != 1) {
+				if (index >= 3 && index < 39 && !this.mergeItemStack(itemstack1, 0, 2, false)) {
+					return ItemStack.EMPTY;
+				}
+			} else if (!this.mergeItemStack(itemstack1, 3, 39, false)) {
+				return ItemStack.EMPTY;
+			}
 
-	         if (itemstack1.isEmpty()) {
-	            slot.putStack(ItemStack.EMPTY);
-	         } else {
-	            slot.onSlotChanged();
-	         }
+			if (itemstack1.isEmpty()) {
+				slot.putStack(ItemStack.EMPTY);
+			} else {
+				slot.onSlotChanged();
+			}
 
-	         if (itemstack1.getCount() == itemstack.getCount()) {
-	            return ItemStack.EMPTY;
-	         }
+			if (itemstack1.getCount() == itemstack.getCount()) {
+				return ItemStack.EMPTY;
+			}
 
-	         ItemStack itemstack2 = slot.onTake(playerIn, itemstack1);
-	         if (index == 0) {
-	            playerIn.dropItem(itemstack2, false);
-	         }
-	      }
+			slot.onTake(playerIn, itemstack1);
+		}
 
-	      return itemstack;
-	   }
+		return itemstack;
+	}
+
+	/**
+	 * Called when the container is closed.
+	 */
+	public void onContainerClosed(PlayerEntity playerIn) {
+		super.onContainerClosed(playerIn);
+		this.canInteractwithCallable.consume((world, blockpos) -> {
+			this.clearContainer(playerIn, world, this.inputSlots);
+		});
+	}
 }
